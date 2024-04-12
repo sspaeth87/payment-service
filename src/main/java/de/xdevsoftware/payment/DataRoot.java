abf320f9fdfc8b81fbff58405094d610a12af8db
Sebastian Spaeth <p.xplugin@gmail.com>
2024-04-12 10:55:32 +0200

    bug fixes.

--- src/main/java/de/xdevsoftware/payment/DataRoot.java
+++ src/main/java/de/xdevsoftware/payment/DataRoot.java
@@ -10,10 +10,6 @@ public class DataRoot
 	private List<Customer> customers;
 
 	public List<Customer> getCustomers() {
-		if(this.customers == null) {
-			this.customers = new ArrayList<>();
-		}
-
 		return this.customers;
 	}
 }
